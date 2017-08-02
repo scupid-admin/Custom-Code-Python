facdf3fd4170acb72159b77d04a144f40e40ca09
ishan <ishan@morph.ai>
2017-08-03 02:01:23 +0530

    adding action

--- morph/action.py
+++ morph/action.py
@@ -1,2 +1,18 @@
+publish = "publish"
+set_variable = ""
+
+
 class Action:
-    def
+    def __init__(self, name):
+        self.name = name
+
+
+class GoToFlow(Action):
+    def __init__(self, flow_title):
+        Action.__init__(name="goToFlow")
+        self.flow_title = flow_title
+
+
+class SetVariable(Action):
+    def __init__(self):
+        Action.__init__(name="setVariableAction")
---
+++ morph/message.py
@@ -0,0 +1,6 @@
+class Message:
+    """
+    Represents a single message to be sent
+    """
+    def __init__(self, message_type):
+        self.message_type = message_type
