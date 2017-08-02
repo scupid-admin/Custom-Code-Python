7901cf5a9ca55075007aa34cbe023079cc401ab7
ishan <ishan@morph.ai>
2017-08-03 02:24:00 +0530

    self

--- morph/action.py
+++ morph/action.py
@@ -1,7 +1,3 @@
-publish = "publish"
-set_variable = ""
-
-
 class Action:
     def __init__(self, name):
         self.name = name
@@ -9,10 +5,15 @@ class Action:
 
 class GoToFlow(Action):
     def __init__(self, flow_title):
-        Action.__init__(name="goToFlow")
+        Action.__init__(self, name="goToFlow")
         self.flow_title = flow_title
 
 
 class SetVariable(Action):
     def __init__(self):
-        Action.__init__(name="setVariableAction")
+        Action.__init__(self, name="setVariableAction")
+
+
+class Publish(Action):
+    def __init__(self):
+        Action.__init__(self, name="publish")
