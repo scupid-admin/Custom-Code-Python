ce9b12ce42dcffc8a3b00472953a92eb79b55c97
ishan <ishan@morph.ai>
2017-08-03 02:03:36 +0530

    documentation

--- morph/message.py
+++ morph/message.py
@@ -1,6 +1,9 @@
 class Message:
     """
     Represents a single message to be sent
+    Parameters
+    ----------
+    message_type : str; one of statement, carousel, list, media
     """
     def __init__(self, message_type):
         self.message_type = message_type
