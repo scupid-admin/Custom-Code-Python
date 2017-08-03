02d3274d08aacba28092fad5cc82a7af8b42f89e
ishan <ishan@morph.ai>
2017-08-04 03:27:35 +0530

    variable json

--- morph/variable.py
+++ morph/variable.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Variable:
     def __init__(self, type):
         self.type = type
@@ -8,14 +11,26 @@ class StringVariable(Variable):
         Variable.__init__(self, "STRING")
         self.value = value
 
+    def to_json(self):
+        attr = {'variableType': self.type, 'value': self.value}
+        return json.dumps(attr)
+
 
 class StringArrayVariable(Variable):
     def __init__(self, values):
         Variable.__init__(self, "STRING_ARRAY")
         self.values = values
 
+    def to_json(self):
+        attr = {'variableType': self.type, 'values': self.values}
+        return json.dumps(attr)
+
 
 class NumberVariable(Variable):
     def __init__(self, value):
         Variable.__init__(self, "DOUBLE")
         self.value = value
+
+    def to_json(self):
+        attr = {'variableType': self.type, 'value': self.value}
+        return json.dumps(attr)
