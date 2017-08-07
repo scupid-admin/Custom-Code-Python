57b9dfe39915597449c2dcb8d835430aded8d3fa
ishan <ishan@morph.ai>
2017-08-07 15:25:08 +0530

    adds .

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author="Ishan Jain",
     author_email='ishan@morph.ai',
     url='https://github.com/scupid-admin/morph-python-sdk',
-    download_url='https://github.com/scupid-admin/morph-python-sdk/archive/1.0.0tar.gz',
+    download_url='https://github.com/scupid-admin/morph-python-sdk/archive/1.0.0.tar.gz',
     keywords = ['chatbots', 'custom code', 'morph.ai'],
     version="1.0.0"
 )
