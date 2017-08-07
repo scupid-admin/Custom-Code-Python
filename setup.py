900d43ff50bc25820e99e58f2e5df71f8b520e06
ishan <ishan@morph.ai>
2017-08-07 15:24:24 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
 setup(
-    name="morph-custom-code",
-    version="1.0.0",
-    scripts="morph"
+    name="morph",
+    packages=['morph'],
+    description='Morph.ai Custom code SDK for python',
+    author="Ishan Jain",
+    author_email='ishan@morph.ai',
+    url='https://github.com/scupid-admin/morph-python-sdk',
+    download_url='https://github.com/scupid-admin/morph-python-sdk/archive/1.0.0tar.gz',
+    keywords = ['chatbots', 'custom code', 'morph.ai'],
+    version="1.0.0"
 )
