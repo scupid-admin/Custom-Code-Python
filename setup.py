a6685087104d3d5b88afb84d6214172a69ec2923
ishan <ishan@morph.ai>
2017-08-07 15:25:18 +0530

    minor

--- setup.py
+++ setup.py
@@ -8,6 +8,6 @@ setup(
     author_email='ishan@morph.ai',
     url='https://github.com/scupid-admin/morph-python-sdk',
     download_url='https://github.com/scupid-admin/morph-python-sdk/archive/1.0.0.tar.gz',
-    keywords = ['chatbots', 'custom code', 'morph.ai'],
+    keywords=['chatbots', 'custom code', 'morph.ai'],
     version="1.0.0"
 )
