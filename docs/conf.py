4dc4fe609fa9001e912e3883d2e0d14121b0ef36
ishan <ishan@morph.ai>
2017-08-26 18:31:31 +0530

    theme change

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,4 @@
+import sphinx_bootstrap_theme
 # -*- coding: utf-8 -*-
 #
 # morph-ai documentation build configuration file, created by
@@ -83,6 +84,8 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
