30b8f82c2717f8db9562b309966e07bc0ddcacea
ishan <ishan@morph.ai>
2017-08-26 15:01:10 +0530

    removing more stuff

--- build/lib/morph/base.py
+++
@@ -1,4 +0,0 @@
-class MorphSerializable:
-    def __init__(self, properties_to_serialize, property_dict):
-        self.property_dict = property_dict
-        self.properties_to_serialize = properties_to_serialize
